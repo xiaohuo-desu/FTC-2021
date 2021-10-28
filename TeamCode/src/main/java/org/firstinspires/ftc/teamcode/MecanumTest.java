6f99efe2387e088219aba45e989719b97974cbcd
Suk <29709893992@qq.com>
2021-10-28 13:54:33 +0800

    MecanumTest上添加了基本功能

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -1,15 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.hardware.Sensor;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
-@TeleOp(name="MecanumTeleOp")
+@TeleOp(name="MecanumTeleOp19")
 public class MecanumTest extends LinearOpMode {
 
     DcMotorEx Leftfront;
@@ -28,9 +31,11 @@ public class MecanumTest extends LinearOpMode {
     double Maxspeed=1;
 
     DcMotor rolling;
-    DcMotor arm;
+    DcMotorEx arm;
 
     Servo catching;
+
+    TouchSensor touchSensor;
 
     public enum Catch{
         Open,Close
@@ -39,20 +44,24 @@ public class MecanumTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        Catch cath=Catch.Open;
+
         Rightback =hardwareMap.get(DcMotorEx.class,"RightBack");
         Leftback = hardwareMap.get(DcMotorEx.class,"LeftBack");
         Rightfront = hardwareMap.get(DcMotorEx.class,"RightFront");
         Leftfront = hardwareMap.get(DcMotorEx.class,"LeftFront");
 
         rolling=hardwareMap.get(DcMotor.class,"rolling");
-        arm=hardwareMap.get(DcMotor.class,"arm");
+        arm=hardwareMap.get(DcMotorEx.class,"arm");
 
         catching=hardwareMap.get(Servo.class,"catching");
 
+        //touchSensor=hardwareMap.get(TouchSensor.class,"TouchSensor");
+
         Leftfront.setDirection(DcMotorSimple.Direction.FORWARD);
-        Rightfront.setDirection(DcMotorSimple.Direction.FORWARD);
+        Rightfront.setDirection(DcMotorSimple.Direction.REVERSE);
         Leftback.setDirection(DcMotorSimple.Direction.FORWARD);
-        Rightback.setDirection(DcMotorSimple.Direction.FORWARD);
+        Rightback.setDirection(DcMotorSimple.Direction.REVERSE);
 
         Leftfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Leftfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -63,7 +72,17 @@ public class MecanumTest extends LinearOpMode {
         Rightback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Rightback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         catching.scaleRange(0,0.6);
+
+        //if(!touchSensor.isPressed()){
+           // arm.setVelocity(-800);
+        //}
+        //else{
+           // arm.setMotorDisable();
+        //}
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -75,6 +94,8 @@ public class MecanumTest extends LinearOpMode {
             telemetry.addData("Status", "Running");
             telemetry.update();
 
+            arm.setMotorEnable();
+
             Left_stick_x=gamepad1.left_stick_x;
             Left_stick_y=gamepad1.left_stick_y;
 
@@ -84,10 +105,10 @@ public class MecanumTest extends LinearOpMode {
 
             double radiantodirection=Math.toRadians(angle);
 
-            flm=(Math.cos(radiantodirection-Math.sin(radiantodirection)))*power;
-            frm=(Math.cos(radiantodirection+Math.sin(radiantodirection)))*power;
-            blm=(Math.cos(radiantodirection+Math.sin(radiantodirection)))*power;
-            brm=(Math.cos(radiantodirection-Math.sin(radiantodirection)))*power;
+            flm=(Math.cos(radiantodirection+Math.sin(radiantodirection)))*power;
+            frm=(Math.cos(radiantodirection-Math.sin(radiantodirection)))*power;
+            blm=(Math.cos(radiantodirection-Math.sin(radiantodirection)))*power;
+            brm=(Math.cos(radiantodirection+Math.sin(radiantodirection)))*power;
 
             double powerreducer=1;
 
@@ -116,15 +137,18 @@ public class MecanumTest extends LinearOpMode {
             blm=Math.round(flm*1000.0)/1000.0;
             brm=Math.round(flm*1000.0)/1000.0;
 
-            Leftfront.setPower(flm);
-            Rightfront.setPower(frm);
-            Leftback.setPower(blm);
-            Rightback.setPower(brm);
+            Leftfront.setPower(flm*0.5);
+            Rightfront.setPower(frm*0.5);
+            Leftback.setPower(blm*0.5);
+            Rightback.setPower(brm*0.5);
 
             //机械臂
-            arm.setPower(gamepad1.left_trigger);
-            arm.setPower(-gamepad1.right_trigger);
+            double Left_trigger=gamepad1.left_trigger*500;
+            double Right_trigger=-gamepad1.right_trigger*500;
+            arm.setVelocity(3000+Left_trigger+Right_trigger);
+
             //转盘
+
             if(gamepad1.right_bumper){
                 rolling.setPower(0.5);
             }
@@ -132,8 +156,19 @@ public class MecanumTest extends LinearOpMode {
                 rolling.setPower(0);
             }
             //爪子
-
-            switch ()
+            switch (cath)
+            {
+                case Open:
+                    if(gamepad1.b)
+                        catching.setPosition(0.6);
+                    cath=Catch.Close;
+                    break;
+                case Close:
+                    if(gamepad1.b)
+                        catching.setPosition(0);
+                    cath=Catch.Open;
+                    break;
+            }
 
         }
     }
